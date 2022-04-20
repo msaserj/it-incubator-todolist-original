b4840bfd45f30e4fce900d0811be2dc5383458fa
msaserj <serg.ks@gmail.com>
2022-04-20 19:48:42 +0300

    02 Second lesson monday. Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,24 @@ import './App.css';
 import {TaskType, Todolist} from "./Todolist";
 
 function App() {
-    const todoListTitle_1 = "What to learn"
-    const todoListTitle_2 = "What to buy"
-    // const todoListTitle_3 = "What to read"
+    const todoListTitle = "What to learn"
 
-    const tasks_1: TaskType[] = [
+    const tasks: TaskType[] = [
         {id: 1, title: "HTML", isDone: true},
         {id: 2, title: "CSS", isDone: true},
         {id: 3, title: "React", isDone: false}
     ]
-const tasks_2: TaskType[] = [
-        {id: 1, title: "Milk", isDone: true},
-        {id: 2, title: "Sugar", isDone: true},
-        {id: 3, title: "Salt", isDone: false}
-    ]
 
-  return (
-    <div className="App">
-      <Todolist
-          title={todoListTitle_1}
-          task={tasks_1}
-      />
-<Todolist
-          title={todoListTitle_2}
-          task={tasks_2}
-      />
+    return (
+        <div className="App">
+            <Todolist
+                title={todoListTitle}
+                task={tasks}
+            />
 
 
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
