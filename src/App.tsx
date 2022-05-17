8864c8b24bbef9d4f6b6bef6ccd0686f2bace7c8
msa <serg.ks@gmail.com>
2022-05-17 18:05:35 +0300

    Lesson 08. start

--- src/App.tsx
+++ src/App.tsx
@@ -125,10 +125,10 @@ function App() {
                     let tasksForTodolist = allTodolistTasks;
 
                     if (tl.filter === "active") {
-                        tasksForTodolist = allTodolistTasks.filter(t => !t.isDone);
+                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
                     }
                     if (tl.filter === "completed") {
-                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone);
+                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
                     }
 
                     return <Todolist
